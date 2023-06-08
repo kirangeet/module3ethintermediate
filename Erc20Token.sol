9afc5c4f9f49583ac7470cb835165298526c8521
kirangeet <50482881+kirangeet@users.noreply.github.com>
2023-06-08 12:11:42 +0530

    Update Erc20Token.sol

--- Erc20Token.sol
+++ Erc20Token.sol
@@ -1,8 +1,15 @@
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.9;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts@4.9.0/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts@4.9.0/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts@4.9.0/access/Ownable.sol";
 
-contract GLDToken is ERC20 {
-    constructor(uint256 initialSupply) ERC20("Gold", "GLD") {
-        _mint(msg.sender, initialSupply);
-    } }
+contract MyToken is ERC20, ERC20Burnable, Ownable {
+    constructor() ERC20("MyToken", "MTK") {
+        _mint(msg.sender, 100 * 10**uint(decimals()));
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
